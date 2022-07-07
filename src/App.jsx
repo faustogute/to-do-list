0b38332227e11c4b6ac5241be58821cb090bad3f
Fausto Gutiérrez Tépoz <faustogute@MacBook-Air-M1.local>
2022-07-06 20:47:01 -0500

    feat: agregar botones para eliminar tareas y la funcion para marcar una tarea como completada o no

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,50 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Tareas from "./components/Tareas";
 
 function App() {
+  const [tareas, setTareas] = useState([
+    { id: 1, texto: "ir por Alonso", fecha: "5 de julio de 2022", done: true },
+    {
+      id: 2,
+      texto: "correr comparativos",
+      fecha: "10 de julio de 2022",
+      done: false,
+    },
+    {
+      id: 3,
+      texto: "preparativos de cumpleaños",
+      fecha: "25 de julio de 2022",
+      done: false,
+    },
+  ]);
+
+  // Borrar una Tarea
+  const deleteTarea = (id) => {
+    setTareas(tareas.filter((tarea) => tarea.id !== id));
+  };
+
+  // Toggle terminado
+  const toggleTerminado = (id) => {
+    setTareas(
+      tareas.map((tarea) =>
+        tarea.id === id ? { ...tarea, done: !tarea.done } : tarea
+      )
+    );
+  };
+
   return (
     <div className="container">
       <Header titulo="Tareas" />
-      <Tareas />
+      {tareas.length > 0 ? (
+        <Tareas
+          tareas={tareas}
+          onDelete={deleteTarea}
+          onToggle={toggleTerminado}
+        />
+      ) : (
+        "No hay tareas para mostrar"
+      )}
     </div>
   );
 }
--- src/components/Tareas.jsx
+++ src/components/Tareas.jsx
@@ -1,24 +1,15 @@
-const tareas = [
-  { id: 1, texto: "ir por Alonso", fecha: "5 de julio de 2022", done: true },
-  {
-    id: 2,
-    texto: "correr comparativos",
-    fecha: "10 de julio de 2022",
-    done: false,
-  },
-  {
-    id: 3,
-    texto: "preparativos de cumpleaños",
-    fecha: "25 de julio de 2022",
-    done: false,
-  },
-];
+import Tarea from "./Tarea";
 
-const Tareas = () => {
+const Tareas = ({ tareas, onDelete, onToggle }) => {
   return (
     <>
       {tareas.map((tarea) => (
-        <h3 key={tarea.id}>{tarea.texto}</h3>
+        <Tarea
+          key={tarea.id}
+          tarea={tarea}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        />
       ))}
     </>
   );
